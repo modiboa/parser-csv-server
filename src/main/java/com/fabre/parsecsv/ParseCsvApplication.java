e300e6fa1696c78fd1258033e15feec70569aa7a
Thiago Fabre <thiago.fabre@letshare.com>
2019-02-24 11:20:55 +0000

    Initial setup

---
+++ src/main/java/com/fabre/parsecsv/ParseCsvApplication.java
@@ -0,0 +1,13 @@
+package com.fabre.parsecsv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParseCsvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParseCsvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabre/parsecsv/ParseCsvApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fabre.parsecsv;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ParseCsvApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
